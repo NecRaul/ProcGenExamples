1dfb49bc53637cba5601d5bab39108bb11d8742d
NecRaul <necraul@kuroneko.dev>
2024-03-14 02:37:58 +0400

    Look And Say sequence

---
+++ LookAndSay/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Text;
+
+string input = "1";
+int iterations = 10;
+int currentIteration = 0;
+char[] characterArray = input.ToCharArray();
+do
+{
+    Console.WriteLine($"Iteration {currentIteration}: {new string(characterArray)}");
+    StringBuilder result = new StringBuilder();
+    char currentCharacter = characterArray[0];
+    int currentLength = 0;
+    for (int i = 0; i < characterArray.Length; i++)
+    {
+        if (currentCharacter == characterArray[i])
+        {
+            currentLength++;
+        }
+        else
+        {
+            result.Append(currentLength);
+            result.Append(currentCharacter);
+            currentLength = 1;
+            currentCharacter = characterArray[i];
+        }
+    }
+    result.Append(currentLength);
+    result.Append(currentCharacter);
+    characterArray = result.ToString().ToCharArray();
+    currentIteration++;
+}
+while (currentIteration <= iterations);
---
+++ LookAndSay/README.md
@@ -0,0 +1,5 @@
+# Look and Say sequence
+
+Conway's Game of Life/Look and Say sequence.
+
+You can change the number of iterations and the starting input in the code.
