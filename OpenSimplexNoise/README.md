0cd254a204f56da472988bf916a46f8c5377ab25
NecRaul <necraul@kuroneko.dev>
2024-04-30 19:08:02 +0400

    Open Simplex Noise

---
+++ OpenSimplexNoise/README.md
@@ -0,0 +1,5 @@
+# OpenSimplex Noise
+
+Procedurally generating NxN sized noise map using OpenSimplex Noise.
+
+You can change the parameters in the code.
---
+++ OpenSimplexNoise/main.py
@@ -0,0 +1,18 @@
+import opensimplex
+import numpy as np
+import matplotlib.pyplot as plt
+import random
+
+size = 1024
+scale = 128
+noise_map = np.zeros((size, size))
+noise = opensimplex.OpenSimplex(seed=random.randrange(1))
+for i in range(size):
+    for j in range(size):
+        noise_map[i, j] = noise.noise2(i / scale, j / scale)
+
+plt.imshow(noise_map, cmap="gray", origin="lower")
+plt.title("OpenSimplex Noise Map")
+plt.tight_layout()
+plt.savefig("OpenSimplex_Noise_Map.png")
+plt.close()
